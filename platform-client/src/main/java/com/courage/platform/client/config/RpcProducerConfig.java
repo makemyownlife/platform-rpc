6cfc028f9ecfdc70cf1f5233fe353e59f8186f96
zhangyong <zhangyong7120180@163.com>
2019-07-05 20:38:47 +0800

    RPC 生产者配置

---
+++ platform-client/src/main/java/com/courage/platform/client/config/RpcProducerConfig.java
@@ -0,0 +1,32 @@
+package com.courage.platform.client.config;
+
+/**
+ * rpc 生产者配置
+ * Created by zhangyong on 2019/7/5.
+ */
+public class RpcProducerConfig {
+
+    //默认监听端口
+    private Integer port = 10029;
+
+    //生产者线程处理数 默认100
+    private Integer threadCount = 100;
+
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Integer getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(Integer threadCount) {
+        this.threadCount = threadCount;
+    }
+
+}
