df53ceed3dd818ca7f814d8fc4635bd29f38bff1
zhangyong <zhangyong7120180@163.com>
2019-07-07 16:44:46 +0800

    应用基础信息

---
+++ platform-client/src/main/java/com/courage/platform/client/config/ApplicationConfig.java
@@ -0,0 +1,30 @@
+package com.courage.platform.client.config;
+
+/**
+ * 应用配置信息
+ * Created by zhangyong on 2019/7/7.
+ */
+public class ApplicationConfig {
+
+    private String appName;
+
+    private String appKey;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey;
+    }
+
+
+}
