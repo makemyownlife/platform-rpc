9344e2c512e4b3b008413fae344a95ff3ca9ceaf
zhangyong <zhangyong7120180@163.com>
2019-07-07 20:55:28 +0800

    远程的生产者信息

--- platform-client/src/main/java/com/courage/platform/client/rpc/impl/RemoteProducerClientImpl.java
+++ platform-client/src/main/java/com/courage/platform/client/rpc/impl/RemoteProducerClientImpl.java
@@ -108,4 +108,9 @@ public class RemoteProducerClientImpl implements RemoteProducerClient {
         remoteRpcThreadPool.shutdown();
     }
 
+    public static void main(String[] args) {
+        RemoteProducerClient remoteProducerClient = new RemoteProducerClientImpl(new RemoteProducerConfig());
+        remoteProducerClient.start();
+    }
+
 }
