277d7e366eb3cc7eb28edef3192ff02459b98eab
zhangyong <zhangyong7120180@163.com>
2019-07-11 20:38:46 +0800

    code

--- platform-client/src/main/java/com/courage/platform/client/rpc/processor/RpcHeartBeatProcessor.java
+++ platform-client/src/main/java/com/courage/platform/client/rpc/processor/RpcHeartBeatProcessor.java
@@ -2,11 +2,8 @@ package com.courage.platform.client.rpc.processor;
 
 import com.courage.platform.rpc.remoting.netty.codec.PlatformNettyRequestProcessor;
 import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommand;
-import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommandFormat;
 import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingSysResponseCode;
 import io.netty.channel.ChannelHandlerContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * 心跳处理器
@@ -14,13 +11,10 @@ import org.slf4j.LoggerFactory;
  */
 public class RpcHeartBeatProcessor implements PlatformNettyRequestProcessor {
 
-    private final static Logger logger = LoggerFactory.getLogger(RpcHeartBeatProcessor.class);
-
     public PlatformRemotingCommand processRequest(ChannelHandlerContext ctx, PlatformRemotingCommand request) throws Exception {
         //直接返回响应码
         PlatformRemotingCommand response = new PlatformRemotingCommand();
         response.setCode(PlatformRemotingSysResponseCode.SUCCESS);
-        response.setFormat(PlatformRemotingCommandFormat.RESPONSE.getCode());
         return response;
     }
 
