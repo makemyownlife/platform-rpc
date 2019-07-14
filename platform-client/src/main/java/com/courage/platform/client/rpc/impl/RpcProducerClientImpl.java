0aabb57d01203b0d80c13d3314022f4d5fad6bbb
zhangyong <zhangyong7120180@163.com>
2019-07-14 10:15:15 +0800

    默认 150个线程

--- platform-client/src/main/java/com/courage/platform/client/rpc/impl/RpcProducerClientImpl.java
+++ platform-client/src/main/java/com/courage/platform/client/rpc/impl/RpcProducerClientImpl.java
@@ -43,7 +43,7 @@ public class RpcProducerClientImpl implements RpcProducerClient {
 
     private final static int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-    private final static int POOL_CORE_SIZE = AVAILABLE_PROCESSORS > 100 ? AVAILABLE_PROCESSORS : 100;
+    private final static int POOL_CORE_SIZE = AVAILABLE_PROCESSORS > 150 ? AVAILABLE_PROCESSORS : 150;
 
     private final static ThreadPoolExecutor remoteRpcThreadPool = new ThreadPoolExecutor(POOL_CORE_SIZE, POOL_CORE_SIZE * 2, 500, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(500000), new ThreadFactory() {
         private AtomicInteger threadIndex = new AtomicInteger(0);
