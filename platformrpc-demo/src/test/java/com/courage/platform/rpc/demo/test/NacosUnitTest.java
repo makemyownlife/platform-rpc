d80da1e9fee68cacf7596c25c6ab465482c35604
zhangyong <zhangyong7120180@163.com>
2019-07-16 23:09:37 +0800

    nacos 单元测试

---
+++ platformrpc-demo/src/test/java/com/courage/platform/rpc/demo/test/NacosUnitTest.java
@@ -0,0 +1,33 @@
+package com.courage.platform.rpc.demo.test;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+
+import java.util.Properties;
+
+/**
+ * Created by zhangyong on 2019/7/16.
+ */
+public class NacosUnitTest {
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        String serverList = "127.0.0.1:8848";
+
+        Properties properties = new Properties();
+        properties.setProperty("serverAddr", serverList);
+        properties.setProperty("namespace", "41f0993c-adbb-4832-a0a7-3f01bc804c99");
+
+        NamingService namingService = NamingFactory.createNamingService(serverList);
+        namingService.registerInstance("nacos.test.3", "11.11.11.11", 8888, "TEST1");
+
+        Instance instance = new Instance();
+        instance.setIp("192.168.1.2");
+        instance.setPort(10229);
+        instance.setEphemeral(false);
+        namingService.registerInstance("mytest", instance);
+        Thread.sleep(10000);
+    }
+
+}
